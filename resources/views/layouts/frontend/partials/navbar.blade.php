ef671414119f0821c582669fc8709bf33a422908
Raf-Taufiqurrahman <raf.taufiqurrahman@gmail.com>
2022-10-29 23:26:44 +0800

    [desgin] update navbar

---
+++ resources/views/layouts/frontend/partials/navbar.blade.php
@@ -0,0 +1,233 @@
+<div class="w-full bg-cyan-800 p-3 border-b border-cyan-700">
+    <div class="container mx-auto">
+        <div class="flex items-center justify-between">
+            <!-- NavMenu -->
+            <a href="" class="font-semibold text-white text-lg flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-tabler w-10 h-10"
+                    width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor"
+                    fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M8 9l3 3l-3 3"></path>
+                    <line x1="13" y1="15" x2="16" y2="15"></line>
+                    <rect x="4" y="4" width="16" height="16" rx="4"></rect>
+                </svg>
+                Larastore
+            </a>
+            <input class="rounded-md border p-2 text-xs w-60" placeholder="Cari Berdasarkan produk nama">
+        </div>
+    </div>
+</div>
+<div class="w-full bg-white p-3 shadow">
+    <div class="container mx-auto">
+        <div class="hidden md:flex items-center justify-between">
+            <ul class="flex items-center justify-center gap-5">
+                <li>
+                    <a href="/" class="flex gap-1 items-center text-gray-700 font-semibold">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home w-5 h-5"
+                            width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <polyline points="5 12 3 12 12 3 21 12 19 12"></polyline>
+                            <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+                            <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path>
+                        </svg>
+                        Beranda
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="flex gap-1 items-center text-gray-700 font-semibold">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-book-2 w-5 h-5"
+                            width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M19 4v16h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12z"></path>
+                            <path d="M19 16h-12a2 2 0 0 0 -2 2"></path>
+                            <path d="M9 8h6"></path>
+                        </svg>
+                        Katalog
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-caret-down fill-bladck w-5 h-5" width="24"
+                            height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M6 10l6 6l6 -6h-12"></path>
+                        </svg>
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="flex gap-1 items-center text-gray-700 font-semibold">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bolt w-5 h-5"
+                            width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor"
+                            fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <polyline points="13 3 13 10 19 10 11 21 11 14 5 14 13 3"></polyline>
+                        </svg>
+                        Flash Sale
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="flex gap-1 items-center text-gray-700 font-semibold">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-address-book w-5 h-5" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M20 6v12a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2z">
+                            </path>
+                            <path d="M10 16h6"></path>
+                            <circle cx="13" cy="11" r="2"></circle>
+                            <path d="M4 8h3"></path>
+                            <path d="M4 12h3"></path>
+                            <path d="M4 16h3"></path>
+                        </svg>
+                        Kontak
+                    </a>
+                </li>
+                <li>
+                    <a href="/" class="flex gap-1 items-center text-gray-700 font-semibold">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-info-circle w-5 h-5" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="12" cy="12" r="9"></circle>
+                            <line x1="12" y1="8" x2="12.01" y2="8"></line>
+                            <polyline points="11 12 12 12 12 16 13 16"></polyline>
+                        </svg>
+                        Tentang
+                    </a>
+                </li>
+            </ul>
+            <ul class="flex items-center justify-center gap-2">
+                <li>
+                    <a href="/"
+                        class="text-white font-semibold flex items-center px-4 py-2 gap-1 rounded-lg text-xs bg-cyan-700">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="icon icon-tabler icon-tabler-user-check w-5 h-5" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="9" cy="7" r="4"></circle>
+                            <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+                            <path d="M16 11l2 2l4 -4"></path>
+                        </svg>
+                        Masuk
+                    </a>
+                </li>
+                <li>
+                    <a href="/"
+                        class="text-white font-semibold flex items-center px-4 py-2 gap-1 rounded-lg text-xs bg-gray-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-plus w-5 h-5"
+                            width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
+                            stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="9" cy="7" r="4"></circle>
+                            <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
+                            <path d="M16 11h6m-3 -3v6"></path>
+                        </svg>
+                        Daftar
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="flex justify-end gap-1 items-center md:hidden">
+            <div class="text-white flex gap-2" x-data="{ isOpen: false }">
+                @guest
+                    <button @click="isOpen = !isOpen" @keydown.escape="isOpen = false"
+                        class="flex items-center gap-2 border rounded-lg px-4 py-2 text-gray-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" x-show="!isOpen"
+                            class="icon icon-tabler icon-tabler-align-right w-5 h-5" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <line x1="4" y1="6" x2="20" y2="6"></line>
+                            <line x1="10" y1="12" x2="20" y2="12"></line>
+                            <line x1="6" y1="18" x2="20" y2="18"></line>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" x-cloak x-show="isOpen"
+                            class="icon icon-tabler icon-tabler-x w-5 h-5" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="1.25" stroke="currentColor" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <line x1="18" y1="6" x2="6" y2="18"></line>
+                            <line x1="6" y1="6" x2="18" y2="18"></line>
+                        </svg>
+                    </button>
+                @endguest
+                <ul x-cloak x-show="isOpen" @click.away="isOpen = false"
+                    class="flex overflow-x-auto w-40 overflow-hidden text-gray-700">
+                    <li>
+                        <a href=""
+                            class="p-3 rounded-lg text-sm font-semibold flex items-center gap-2 hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home w-5 h-5"
+                                width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <polyline points="5 12 3 12 12 3 21 12 19 12"></polyline>
+                                <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+                                <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path>
+                            </svg>
+                            Beranda
+                        </a>
+                    </li>
+                    <li>
+                        <a href=""
+                            class="p-3 rounded-lg text-sm font-semibold flex items-center gap-2 hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home w-5 h-5"
+                                width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <polyline points="5 12 3 12 12 3 21 12 19 12"></polyline>
+                                <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+                                <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path>
+                            </svg>
+                            Katalog
+                        </a>
+                    </li>
+                    <li>
+                        <a href=""
+                            class="p-3 rounded-lg text-sm font-semibold flex items-center gap-2 hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home w-5 h-5"
+                                width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <polyline points="5 12 3 12 12 3 21 12 19 12"></polyline>
+                                <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+                                <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path>
+                            </svg>
+                            Flash Sale
+                        </a>
+                    </li>
+                    <li>
+                        <a href=""
+                            class="p-3 rounded-lg text-sm font-semibold flex items-center gap-2 hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home w-5 h-5"
+                                width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <polyline points="5 12 3 12 12 3 21 12 19 12"></polyline>
+                                <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+                                <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path>
+                            </svg>
+                            Kontak
+                        </a>
+                    </li>
+                    <li>
+                        <a href=""
+                            class="p-3 rounded-lg text-sm font-semibold flex items-center gap-2 hover:text-blue-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home w-5 h-5"
+                                width="24" height="24" viewBox="0 0 24 24" stroke-width="1.25"
+                                stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <polyline points="5 12 3 12 12 3 21 12 19 12"></polyline>
+                                <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+                                <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"></path>
+                            </svg>
+                            Kontak
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
